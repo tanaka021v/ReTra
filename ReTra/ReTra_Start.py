8045d48021fd72c2946abf91a41d84a95ae96427
Tanaka <126285780+tanaka021v@users.noreply.github.com>
2023-08-11 12:03:20 +0200

    Update and rename ReTra Algorithm.py to ReTra_Start.py

--- ReTra/ReTra_Start.py
+++ ReTra/ReTra_Start.py
@@ -7,8 +7,8 @@ def start_RETRA():
     output1 = '12'   #Hyper Virtual Headphone
     output2 = '14'   #Freedom Headphone
     
-    subprocess.Popen(['python', 'Retra1.py', language1, language2, output2, input1 ])
-    subprocess.Popen(['python', 'Retra1.py', language2, language1, output1, input2 ])
+    subprocess.Popen(['python', 'RetraAlgorithm.py', language1, language2, output2, input1 ])
+    subprocess.Popen(['python', 'RetraAlgorithm.py', language2, language1, output1, input2 ])
 
 if __name__ == '__main__':
     start_RETRA()
